4df06f1fb95673cb1f95e330462b2aed2ae50940
Kushagra Singh <121503206+kushagraSingh100@users.noreply.github.com>
2025-04-20 12:18:33 +0530

    Update multiple_inference.py

--- multiple_inference.py
+++ multiple_inference.py
@@ -1,41 +1,30 @@
-from PIL import Image
-from torchvision import transforms
-import torch
-
 import torch
 from model.MyNet import MyNet
+from data import test_dataset
 import os
 
 # Initialize model and load weights
-model = MyNet()  # include any args your model needs
+model = MyNet()  
 model.load_state_dict(torch.load('MyNet.pth'))
 model.eval()
 model.cuda() if torch.cuda.is_available() else model.cpu()
 
-# Image transform (same as training)
-transform = transforms.Compose([
-    transforms.Resize((352, 352)),  # or your training size
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
 
-img_path = 'EORSSD/test-images/0023.jpg'
-img = Image.open(img_path).convert('RGB')
-input_tensor = transform(img).unsqueeze(0)
+# Directory paths
+image_root = 'examples/test-images'  
+gt_root = 'examples/test-labels' 
+testsize = 352  
 
-input_tensor = input_tensor.cuda() if torch.cuda.is_available() else input_tensor
-model.eval()
-with torch.no_grad():
-    pred,_ = model(input_tensor)
-    pred = torch.sigmoid(pred)
-    pred = pred.squeeze().cpu().numpy()
+test_data = test_dataset(image_root, gt_root, testsize)
 
-# Save or view prediction
-pred_img = Image.fromarray((pred * 255).astype('uint8'))
-pred_img.save('prediction.png')
+for i in range(test_data.size):
+    image, gt, name = test_data.load_data()
+    image = image.cuda() if torch.cuda.is_available() else image
+    with torch.no_grad():
+        prediction,_ = model(image)
+        prediction = torch.sigmoid(prediction) 
+        pred_np = prediction.squeeze().cpu().numpy()  
 
-# model = MyNet()
-# total_params = sum(p.numel() for p in model.parameters())
-# trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-# print(f"Total parameters: {total_params}")
-# print(f"Trainable parameters: {trainable_params}")
+    from PIL import Image
+    pred_img = Image.fromarray((pred_np * 255).astype('uint8'))
+    pred_img.save(os.path.join('examples/results/', name))  
